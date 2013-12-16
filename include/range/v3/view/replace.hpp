71ace8c865de1b73180a2f85313dda9608d62c1b
Eric Niebler <eric@boostpro.com>
2013-12-15 22:42:02 -0800

    add replace view

---
+++ include/range/v3/view/replace.hpp
@@ -0,0 +1,91 @@
+// Boost.Range library
+//
+//  Copyright Thorsten Ottosen, Neil Groves 2006 - 2008.
+//  Copyright Eric Niebler 2013.
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org/libs/range/
+//
+
+#ifndef RANGES_V3_VIEW_REPLACE_HPP
+#define RANGES_V3_VIEW_REPLACE_HPP
+
+#include <utility>
+#include <type_traits>
+#include <range/v3/range_fwd.hpp>
+#include <range/v3/view/transform.hpp>
+#include <range/v3/utility/bindable.hpp>
+#include <range/v3/utility/concepts.hpp>
+
+namespace ranges
+{
+    inline namespace v3
+    {
+        namespace view
+        {
+            struct replacer : bindable<replacer>
+            {
+            private:
+                template<typename Value>
+                struct replacer_fun
+                {
+                private:
+                    friend struct replacer;
+                    Value old_value_;
+                    Value new_value_;
+
+                    template<typename Value1, typename Value2>
+                    replacer_fun(Value1 && old_value, Value2 && new_value)
+                      : old_value_(detail::forward<Value1>(old_value)),
+                        new_value_(detail::forward<Value2>(new_value))
+                    {}
+
+                public:
+                    template<typename Other>
+                    Other operator()(Other && other) const
+                    {
+                        return (other == old_value_) ? new_value_ : std::forward<Other>(other);
+                    }
+                };
+            public:
+                template<typename InputRange, typename Value1, typename Value2,
+                    CONCEPT_REQUIRES(ranges::SameType<typename std::decay<Value1>::type,
+                                                      typename std::decay<Value2>::type>())>
+                static transform_range_view<InputRange,
+                                            replacer_fun<typename std::decay<Value1>::type>>
+                invoke(replacer, InputRange && rng, Value1 && old_value, Value2 && new_value)
+                {
+                    CONCEPT_ASSERT(ranges::InputRange<InputRange>());
+                    CONCEPT_ASSERT(ranges::EqualityComparable<range_reference_t<InputRange>,
+                        typename std::decay<Value1>::type const &>());
+                    CONCEPT_ASSERT(ranges::Convertible<typename std::decay<Value1>::type const &,
+                        range_reference_t<InputRange>>());
+                    return {detail::forward<InputRange>(rng),
+                            {detail::forward<Value1>(old_value),
+                             detail::forward<Value2>(new_value)}};
+
+                }
+
+                /// \overload
+                template<typename Value1, typename Value2,
+                    CONCEPT_REQUIRES(ranges::SameType<typename std::decay<Value1>::type,
+                                                      typename std::decay<Value2>::type>())>
+                static auto invoke(replacer replace, Value1 && old_value, Value2 && new_value) ->
+                    decltype(replace(std::placeholders::_1, detail::forward<Value1>(old_value),
+                        detail::forward<Value2>(new_value)))
+                {
+                    return replace(std::placeholders::_1, detail::forward<Value1>(old_value),
+                        detail::forward<Value2>(new_value));
+                }
+            };
+
+            RANGES_CONSTEXPR replacer replace {};
+        }
+    }
+}
+
+#endif
