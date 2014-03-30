fde52790474d8edd7691f153cb3f2ce05c515a92
Andrew Sutton <andrew.n.sutton@gmail.com>
2014-03-30 07:38:46 -0400

    Adding CMake build.
    Stubbing out test for all_of algorithm.

---
+++ test/algorithm/all_of.cpp
@@ -0,0 +1,15 @@
+//  Copyright Andrew Sutton 2014
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org/libs/range/
+
+#include <range/v3/algorithm.hpp>
+
+int main() 
+{
+
+}
