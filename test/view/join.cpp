ea31d7533a601f02bb6b898075f4a38d05351f32
Eric Niebler <eniebler@boost.org>
2014-07-10 21:53:17 -0700

    quick-n-dirty tests for join and stride view

---
+++ test/view/join.cpp
@@ -0,0 +1,68 @@
+//  Copyright Eric Niebler 2014
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org/libs/range/
+
+#include <vector>
+#include <range/v3/core.hpp>
+#include <range/v3/view/join.hpp>
+#include <range/v3/view/reverse.hpp>
+#include "../simple_test.hpp"
+#include "../test_utils.hpp"
+
+int main()
+{
+    using namespace ranges;
+
+    std::vector<std::string> his_face{"this", "is", "his", "face"};
+    std::vector<std::string> another_mess{"another", "fine", "mess"};
+    auto joined = view::join(his_face, another_mess);
+    ::models<concepts::RandomAccessIterable>(joined);
+    static_assert(std::is_same<range_reference_t<decltype(joined)>, std::string &>::value, "");
+    CHECK(joined.size() == 7);
+    CHECK((joined.end() - joined.begin()) == 7);
+    ::check_equal(joined | view::reverse, {"mess", "fine", "another", "face", "his", "is", "this"});
+
+    auto revjoin = joined | view::reverse;
+    CHECK((revjoin.end() - revjoin.begin()) == 7);
+
+    auto begin = joined.begin();
+    CHECK(*(begin+0) == "this");
+    CHECK(*(begin+1) == "is");
+    CHECK(*(begin+2) == "his");
+    CHECK(*(begin+3) == "face");
+    CHECK(*(begin+4) == "another");
+    CHECK(*(begin+5) == "fine");
+    CHECK(*(begin+6) == "mess");
+
+    CHECK(*(begin) == "this");
+    CHECK(*(begin+=1) == "is");
+    CHECK(*(begin+=1) == "his");
+    CHECK(*(begin+=1) == "face");
+    CHECK(*(begin+=1) == "another");
+    CHECK(*(begin+=1) == "fine");
+    CHECK(*(begin+=1) == "mess");
+
+    auto end = joined.end();
+    CHECK(*(end-1) == "mess");
+    CHECK(*(end-2) == "fine");
+    CHECK(*(end-3) == "another");
+    CHECK(*(end-4) == "face");
+    CHECK(*(end-5) == "his");
+    CHECK(*(end-6) == "is");
+    CHECK(*(end-7) == "this");
+
+    CHECK(*(end-=1) == "mess");
+    CHECK(*(end-=1) == "fine");
+    CHECK(*(end-=1) == "another");
+    CHECK(*(end-=1) == "face");
+    CHECK(*(end-=1) == "his");
+    CHECK(*(end-=1) == "is");
+    CHECK(*(end-=1) == "this");
+
+    return test_result();
+}
---
+++ test/view/stride.cpp
@@ -0,0 +1,64 @@
+//  Copyright Eric Niebler 2014
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org/libs/range/
+
+#include <list>
+#include <vector>
+#include <sstream>
+#include <range/v3/core.hpp>
+#include <range/v3/istream_range.hpp>
+#include <range/v3/view/stride.hpp>
+#include <range/v3/view/reverse.hpp>
+#include <range/v3/algorithm/copy.hpp>
+#include <range/v3/utility/iterator.hpp>
+#include <range/v3/numeric.hpp>
+#include "../simple_test.hpp"
+#include "../test_utils.hpp"
+
+int main()
+{
+    using namespace ranges;
+
+    std::vector<int> v(50);
+    iota(v, 0);
+
+    static_assert(
+        sizeof((v|view::stride(3)).begin()) ==
+        sizeof(void*)+sizeof(v.begin())+sizeof(std::ptrdiff_t),"");
+    ::check_equal(v | view::stride(3) | view::reverse,
+                  {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
+
+    std::stringstream str;
+    copy(v, ostream_iterator<int>{str, " "});
+    auto x = istream<int>(str) | view::stride(3);
+    CONCEPT_ASSERT(InputIterable<decltype(x)>());
+    check_equal(x, {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
+
+    std::list<int> li;
+    copy(v, back_inserter(li));
+    ::check_equal(li | view::stride(3),
+                  {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
+    ::check_equal(li | view::stride(3) | view::reverse,
+                  {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
+
+    for(int i : li | view::stride(3) | view::reverse)
+        std::cout << i << ' ';
+    std::cout << '\n';
+
+    auto x2 = v | view::stride(3);
+    CHECK(ranges::distance(x2) == 17);
+
+    auto it0 = x2.begin();
+    auto it1 = std::next(it0, 10);
+    CHECK((it1 - it0) == 10);
+    CHECK((it0 - it1) == -10);
+    CHECK((it0 - it0) == 0);
+    CHECK((it1 - it1) == 0);
+
+    return ::test_result();
+}
