0fd9a79e8bb8e0d62eade3df4029f30c8cfa630d
gnzlbg <gonzalobg88@gmail.com>
2014-07-14 13:59:21 +0200

    take_while implementation

--- test/view/take_while.cpp
+++ test/view/take_while.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 #include <range/v3/core.hpp>
 #include <range/v3/view/iota.hpp>
-#include <range/v3/view/while_true.hpp>
+#include <range/v3/view/take_while.hpp>
 #include <range/v3/algorithm/for_each.hpp>
 #include "../simple_test.hpp"
 #include "../test_utils.hpp"
@@ -18,18 +18,15 @@
 int main()
 {
     using namespace ranges;
-    // TODO: if lambda is passed to while_true, rng0 does not model the Iterable
-    // concept anymore
-    std::function<bool(int)> pred = [](auto i) { return i != 25; };
-    auto rng0 = view::iota(10) | view::while_true(pred);
+    auto rng0 = view::iota(10) | view::take_while([](auto i) { return i != 25; });
     ::check_equal(rng0, {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24});
     ::models<concepts::Iterable>(rng0);
     ::models_not<concepts::Range>(rng0);
     ::models<concepts::RandomAccessIterator>(rng0.begin());
-    CONCEPT_ASSERT(RandomAccessIterable<while_trueed_view<std::vector<int>, std::function<bool(int)>>>());
+    CONCEPT_ASSERT(RandomAccessIterable<take_whileed_view<std::vector<int>, std::function<bool(int)>>>());
 
     std::vector<int> vi{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    auto rng1 = vi | view::while_true([](auto i) { return i != 50; });
+    auto rng1 = vi | view::take_while([](auto i) { return i != 50; });
     ::check_equal(rng1, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
 
     return test_result();
