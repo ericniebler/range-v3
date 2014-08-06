d27bf450ca76c178f2ccf36cbb82083e63a67833
Eric Niebler <eniebler@boost.org>
2014-08-05 19:53:58 -0700

    test pop_heap

--- include/range/v3/algorithm/heap_algorithm.hpp
+++ include/range/v3/algorithm/heap_algorithm.hpp
@@ -1,7 +1,7 @@
 //  Copyright Eric Niebler 2014
 //
 //  Use, modification and distribution is subject to the
-//  Boost Software License, Version 1.0. (See acpredanying
+//  Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
 //
