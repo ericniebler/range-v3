5eae544c747c8561656248073bcdab2ab9d3160f
Eric Niebler <eniebler@boost.org>
2014-09-27 09:45:45 -0700

    partial_sort and tests

--- include/range/v3/algorithm/partial_sort.hpp
+++ include/range/v3/algorithm/partial_sort.hpp
@@ -12,15 +12,54 @@
 #ifndef RANGES_V3_ALGORITHM_PARTIAL_SORT_HPP
 #define RANGES_V3_ALGORITHM_PARTIAL_SORT_HPP
 
+#include <functional>
 #include <range/v3/range_fwd.hpp>
 #include <range/v3/begin_end.hpp>
 #include <range/v3/range_concepts.hpp>
 #include <range/v3/range_traits.hpp>
+#include <range/v3/utility/iterator.hpp>
+#include <range/v3/utility/iterator_concepts.hpp>
+#include <range/v3/utility/iterator_traits.hpp>
+#include <range/v3/algorithm/heap_algorithm.hpp>
 
 namespace ranges
 {
     inline namespace v3
     {
+        struct partial_sort_fn
+        {
+            template<typename I, typename S, typename C = ordered_less, typename P = ident,
+                CONCEPT_REQUIRES_(Sortable<I, C, P>() && RandomAccessIterator<I>() && IteratorRange<I, S>())>
+            I operator()(I begin, I middle, S end, C pred_ = C{}, P proj_ = P{}) const
+            {
+                auto && pred = invokable(pred_);
+                auto && proj = invokable(proj_);
+
+                make_heap(begin, middle, std::ref(pred), std::ref(proj));
+                auto const len = middle - begin;
+                I i = middle;
+                for(; i != end; ++i)
+                {
+                    if(pred(proj(*i), proj(*begin)))
+                    {
+                        iter_swap(i, begin);
+                        detail::sift_down_n(begin, len, begin, std::ref(pred), std::ref(proj));
+                    }
+                }
+                sort_heap(begin, middle, std::ref(pred), std::ref(proj));
+                return i;
+            }
+
+            template<typename Rng, typename C = ordered_less, typename P = ident,
+                typename I = range_iterator_t<Rng>,
+                CONCEPT_REQUIRES_(Sortable<I, C, P>() && RandomAccessIterable<Rng>())>
+            I operator()(Rng & rng, I middle, C pred = C{}, P proj = P{}) const
+            {
+                return (*this)(begin(rng), std::move(middle), end(rng), std::move(pred), std::move(proj));
+            }
+        };
+
+        RANGES_CONSTEXPR partial_sort_fn partial_sort {};
 
     } // namespace v3
 } // namespace ranges
---
+++ test/algorithm/partial_sort.cpp
@@ -0,0 +1,163 @@
+// Range v3 library
+//
+//  Copyright Eric Niebler 2014
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cassert>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <range/v3/core.hpp>
+#include <range/v3/algorithm/partial_sort.hpp>
+#include "../simple_test.hpp"
+#include "../test_utils.hpp"
+#include "../test_iterators.hpp"
+
+struct indirect_less
+{
+    template <class P>
+    bool operator()(const P& x, const P& y)
+        {return *x < *y;}
+};
+
+void
+test_larger_sorts(int N, int M)
+{
+    assert(N > 0);
+    assert(M >= 0 && M <= N);
+    int* array = new int[N];
+    for(int i = 0; i < N; ++i)
+        array[i] = i;
+
+    using I = random_access_iterator<int*>;
+    using S = sentinel<int*>;
+
+    std::random_shuffle(array, array+N);
+    int *res = ranges::partial_sort(array, array+M, array+N);
+    CHECK(res == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == i);
+
+    std::random_shuffle(array, array+N);
+    I res2 = ranges::partial_sort(I{array}, I{array+M}, S{array+N});
+    CHECK(res2.base() == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == i);
+
+    std::random_shuffle(array, array+N);
+    res = ranges::partial_sort(::as_lvalue(ranges::make_range(array, array+N)), array+M);
+    CHECK(res == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == i);
+
+    std::random_shuffle(array, array+N);
+    res2 = ranges::partial_sort(::as_lvalue(ranges::make_range(I{array}, S{array+N})), I{array+M});
+    CHECK(res2.base() == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == i);
+
+    std::random_shuffle(array, array+N);
+    res = ranges::partial_sort(array, array+M, array+N, std::greater<int>());
+    CHECK(res == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == N-i-1);
+
+    std::random_shuffle(array, array+N);
+    res2 = ranges::partial_sort(I{array}, I{array+M}, S{array+N}, std::greater<int>());
+    CHECK(res2.base() == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == N-i-1);
+
+    std::random_shuffle(array, array+N);
+    res = ranges::partial_sort(::as_lvalue(ranges::make_range(array, array+N)), array+M, std::greater<int>());
+    CHECK(res == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == N-i-1);
+
+    std::random_shuffle(array, array+N);
+    res2 = ranges::partial_sort(::as_lvalue(ranges::make_range(I{array}, S{array+N})), I{array+M}, std::greater<int>());
+    CHECK(res2.base() == array+N);
+    for(int i = 0; i < M; ++i)
+        CHECK(array[i] == N-i-1);
+
+    delete [] array;
+}
+
+void
+test_larger_sorts(int N)
+{
+    test_larger_sorts(N, 0);
+    test_larger_sorts(N, 1);
+    test_larger_sorts(N, 2);
+    test_larger_sorts(N, 3);
+    test_larger_sorts(N, N/2-1);
+    test_larger_sorts(N, N/2);
+    test_larger_sorts(N, N/2+1);
+    test_larger_sorts(N, N-2);
+    test_larger_sorts(N, N-1);
+    test_larger_sorts(N, N);
+}
+
+struct S
+{
+    int i, j;
+};
+
+int main()
+{
+    int i = 0;
+    int * res = ranges::partial_sort(&i, &i, &i);
+    CHECK(i == 0);
+    CHECK(res == &i);
+    test_larger_sorts(10);
+    test_larger_sorts(256);
+    test_larger_sorts(257);
+    test_larger_sorts(499);
+    test_larger_sorts(500);
+    test_larger_sorts(997);
+    test_larger_sorts(1000);
+    test_larger_sorts(1009);
+
+    // Check move-only types
+    {
+        std::vector<std::unique_ptr<int> > v(1000);
+        for(int i = 0; (std::size_t)i < v.size(); ++i)
+            v[i].reset(new int(v.size() - i - 1));
+        ranges::partial_sort(v, v.begin() + v.size()/2, indirect_less());
+        for(int i = 0; (std::size_t)i < v.size()/2; ++i)
+            CHECK(*v[i] == i);
+    }
+
+    // Check projections
+    {
+        std::vector<S> v(1000, S{});
+        for(int i = 0; (std::size_t)i < v.size(); ++i)
+        {
+            v[i].i = v.size() - i - 1;
+            v[i].j = i;
+        }
+        ranges::partial_sort(v, v.begin() + v.size()/2, std::less<int>{}, &S::i);
+        for(int i = 0; (std::size_t)i < v.size()/2; ++i)
+        {
+            CHECK(v[i].i == i);
+            CHECK((std::size_t)v[i].j == v.size() - i - 1);
+        }
+    }
+
+    return ::test_result();
+}
