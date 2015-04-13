1db9c555603793579ad164bf8929713ff2a67bc2
Eric Niebler <eric@boostpro.com>
2015-04-13 12:54:24 -0700

    Add getlines range for reading lines from an istream

---
+++ include/range/v3/getlines.hpp
@@ -0,0 +1,96 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Eric Niebler 2013-2014
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+
+#ifndef RANGES_V3_GETLINES_HPP
+#define RANGES_V3_GETLINES_HPP
+
+#include <string>
+#include <istream>
+#include <range/v3/range_fwd.hpp>
+#include <range/v3/range_facade.hpp>
+#include <range/v3/utility/static_const.hpp>
+
+namespace ranges
+{
+    inline namespace v3
+    {
+        /// \addtogroup group-core
+        /// @{
+        struct getlines_range
+          : range_facade<getlines_range>
+        {
+        private:
+            friend range_access;
+            std::istream *sin_;
+            std::string str_;
+            char delim_;
+            struct cursor
+            {
+            private:
+                getlines_range *rng_;
+            public:
+                cursor() = default;
+                explicit cursor(getlines_range &rng)
+                  : rng_(&rng)
+                {}
+                void next()
+                {
+                    rng_->next();
+                }
+                std::string const &current() const
+                {
+                    return rng_->str_;
+                }
+                bool done() const
+                {
+                    return !*rng_->sin_;
+                }
+            };
+            void next()
+            {
+                std::getline(*sin_, str_, delim_);
+            }
+            cursor begin_cursor()
+            {
+                return cursor{*this};
+            }
+        public:
+            getlines_range() = default;
+            getlines_range(std::istream &sin, char delim = '\n')
+              : sin_(&sin), str_{}, delim_(delim)
+            {
+                this->next(); // prime the pump
+            }
+            std::string & cached()
+            {
+                return str_;
+            }
+        };
+
+        struct getlines_fn
+        {
+            getlines_range operator()(std::istream & sin, char delim = '\n') const
+            {
+                return getlines_range{sin, delim};
+            }
+        };
+
+        namespace
+        {
+            constexpr auto && getlines = static_const<getlines_fn>::value;
+        }
+        /// @}
+    }
+}
+
+#endif
---
+++ test/getlines.cpp
@@ -0,0 +1,33 @@
+// Range v3 library
+//
+//  Copyright Eric Niebler 2014
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+
+#include <sstream>
+#include <range/v3/core.hpp>
+#include "./simple_test.hpp"
+#include "./test_utils.hpp"
+
+int main()
+{
+    std::stringstream sin{
+R"(Now is
+the time
+for all
+good men
+)"};
+
+    ::check_equal(ranges::getlines(sin), {"Now is", "the time", "for all", "good men"});
+
+    using Rng = decltype(ranges::getlines(sin));
+    CONCEPT_ASSERT(ranges::InputRange<Rng>());
+    CONCEPT_ASSERT(!ranges::ForwardRange<Rng>());
+
+    return ::test_result();
+}
