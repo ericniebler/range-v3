79d52ac365a8e2708206db977d6f52236f6c18eb
Casey Carter <Casey@Carter.net>
2015-05-24 16:02:30 -0500

    Reservable should refine Container, not LvalueContainerLike.

--- include/range/v3/action/concepts.hpp
+++ include/range/v3/action/concepts.hpp
@@ -77,6 +77,33 @@ namespace ranges
         template<typename T>
         using Container = concepts::models<concepts::Container, T>;
 
+        namespace concepts
+        {
+            struct Reservable : refines<Container> {
+                template <class T>
+                using size_type = decltype(std::declval<const T&>().size());
+
+                template <class C>
+                auto requires_(C&& c) -> decltype(
+                    concepts::model_of<Integral, size_type<C>>(),
+                    concepts::valid_expr((c.reserve(c.size()), 42))
+                );
+            };
+
+            struct RandomAccessReservable : refines<Reservable> {
+                template <class C>
+                auto requires_(C&& c) -> decltype(
+                    concepts::model_of<RandomAccessIterator, decltype(begin(c))>()
+                );
+            };
+        }
+
+        template <class C>
+        using Reservable = concepts::models<concepts::Reservable, C>;
+
+        template <class C>
+        using RandomAccessReservable = concepts::models<concepts::RandomAccessReservable, C>;
+
         /// \cond
         namespace detail
         {
@@ -102,35 +129,10 @@ namespace ranges
                         detail::is_lvalue_container_like(std::forward<T>(t))
                     ));
             };
-
-            struct Reservable : refines<LvalueContainerLike> {
-                template <class T>
-                using size_type = decltype(std::declval<const T&>().size());
-
-                template <class C>
-                auto requires_(C&& c) -> decltype(
-                    concepts::model_of<Integral, size_type<C>>(),
-                    concepts::valid_expr((c.reserve(c.size()), 42))
-                );
-            };
-
-            struct RandomAccessReservable : refines<Reservable> {
-                template <class C>
-                auto requires_(C&& c) -> decltype(
-                    concepts::model_of<RandomAccessIterator, decltype(begin(c))>()
-                );
-            };
         }
 
         template<typename T>
         using LvalueContainerLike = concepts::models<concepts::LvalueContainerLike, T>;
-
-        template <class C>
-        using Reservable = concepts::models<concepts::Reservable, C>;
-
-        template <class C>
-        using RandomAccessReservable = concepts::models<concepts::RandomAccessReservable, C>;
-
         /// @}
     }
 }
