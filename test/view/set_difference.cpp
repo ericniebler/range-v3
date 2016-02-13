bc18db5a6d48cc45c06aba4e08ad030a898e5f03
Tomislav Ivek <tomislav.ivek@gmail.com>
2016-02-13 02:04:40 +0100

    test move semantics for view::set_difference

--- test/view/set_difference.cpp
+++ test/view/set_difference.cpp
@@ -9,7 +9,10 @@
 //
 // Project home: https://github.com/ericniebler/range-v3
 
+#include <cstring>
+#include <string>
 #include <vector>
+#include <sstream>
 #include <range/v3/core.hpp>
 #include <range/v3/range_for.hpp>
 #include <range/v3/algorithm/set_algorithm.hpp>
@@ -28,6 +31,44 @@
 #include "../simple_test.hpp"
 #include "../test_utils.hpp"
 
+
+struct MoveOnlyString
+{
+    char const *sz_;
+
+    MoveOnlyString(char const *sz = "")
+    : sz_(sz)
+    {}
+    MoveOnlyString(MoveOnlyString &&that)
+    : sz_(that.sz_)
+    {
+        that.sz_ = "";
+    }
+    MoveOnlyString(MoveOnlyString const &) = delete;
+    MoveOnlyString &operator=(MoveOnlyString &&that)
+    {
+        sz_ = that.sz_;
+        that.sz_ = "";
+        return *this;
+    }
+    MoveOnlyString &operator=(MoveOnlyString const &) = delete;
+    bool operator==(MoveOnlyString const &that) const
+    {
+        return 0 == std::strcmp(sz_, that.sz_);
+    }
+    bool operator<(const MoveOnlyString &that) const
+    {
+        return std::strcmp(sz_, that.sz_) < 0;
+    }
+    bool operator!=(MoveOnlyString const &that) const
+    {
+        return !(*this == that);
+    }
+    friend std::ostream & operator<< (std::ostream &sout, MoveOnlyString const &str)
+    {
+        return sout << '"' << str.sz_ << '"';
+    }
+};
 
 
 int main()
@@ -206,6 +247,27 @@ int main()
                                         );
         CONCEPT_ASSERT(Same<range_value_t<decltype(res2)>, int>());
         ::check_equal(res2, {-2, -1, 0, 2, 4, 5, 7, 9});
+    }
+    
+    
+    // move
+    {
+        auto v0 = to_<std::vector<MoveOnlyString>>({"a","b","c","x"});
+        auto v1 = to_<std::vector<MoveOnlyString>>({"b","x","y","z"});
+        auto res = view::set_symmetric_difference(v0, v1, [](const MoveOnlyString& a, const MoveOnlyString& b){return a<b;});
+
+        std::vector<MoveOnlyString> expected;
+        move(res, back_inserter(expected));
+
+        ::check_equal(expected, {"a","c","y","z"});
+        ::check_equal(v0, {"","b","","x"});
+        ::check_equal(v1, {"b","x","",""});
+ 
+        using R = decltype(res);
+
+        CONCEPT_ASSERT(Same<range_value_t<R>, MoveOnlyString>());
+        CONCEPT_ASSERT(Same<range_reference_t<R>, MoveOnlyString &>());
+        CONCEPT_ASSERT(Same<range_rvalue_reference_t<R>, MoveOnlyString &&>());
     }
     
     // WARNING: set_difference between two infinite ranges can create infinite loops!
