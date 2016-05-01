1441ae475ba9f7892709e5b070c7fb71999f0ba9
Eric Niebler <eniebler@boost.org>
2016-04-30 20:42:29 -0700

    Gcc debug iterator fiasco (#344)
    * constrain libstdc++'s debug iterator wrappers for them, fixes #294
    
    * minor tweak

--- include/range/v3/utility/iterator_concepts.hpp
+++ include/range/v3/utility/iterator_concepts.hpp
@@ -615,4 +615,26 @@ namespace ranges
     }
 }
 
+
+#ifdef _GLIBCXX_DEBUG
+// HACKHACK: workaround underconstrained operator- for libstdc++ debug iterator wrapper
+// by intentionally creating an ambiguity when the wrapped types don't support the
+// necessary operation.
+#include <debug/safe_iterator.h>
+
+namespace __gnu_debug
+{
+    template <class I1, class I2, class Seq,
+        CONCEPT_REQUIRES_(!::ranges::SizedIteratorRange<I1, I2>())>
+    ::ranges::iterator_difference_t<I1>
+        operator-(_Safe_iterator<I1, Seq> const &, _Safe_iterator<I2, Seq> const &) = delete;
+
+    template <class I1, class Seq,
+        CONCEPT_REQUIRES_(!::ranges::SizedIteratorRange<I1, I1>())>
+    ::ranges::iterator_difference_t<I1>
+        operator-(_Safe_iterator<I1, Seq> const &, _Safe_iterator<I1, Seq> const &) = delete;
+}
+
+#endif
+
 #endif // RANGES_V3_UTILITY_ITERATOR_CONCEPTS_HPP
