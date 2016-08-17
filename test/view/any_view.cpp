360d4085db60c6eee7615f74de1ab25db3508775
Casey Carter <Casey@Carter.net>
2016-08-17 10:45:42 -0700

    Make any_view<Ref> = Rng work ... when ConvertibleTo<range_reference_t<Rng>, Ref>()
    The reference type of any_cursor_impl and any_view_impl is explicit, and the reference type of the actual iterator / view must convert to the explicit type.
    
    Drive-by: the distance between value-initialized iterators must be 0.

--- test/view/any_view.cpp
+++ test/view/any_view.cpp
@@ -9,6 +9,7 @@
 //
 // Project home: https://github.com/ericniebler/range-v3
 
+#include <vector>
 #include <range/v3/core.hpp>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/take.hpp>
@@ -21,34 +22,58 @@
 int main()
 {
     using namespace ranges;
+    auto const ten_ints = {0,1,2,3,4,5,6,7,8,9};
 
-    any_view<int> ints = view::ints;
-    ::models<concepts::InputView>(ints);
-    ::models_not<concepts::ForwardView>(ints);
-    ::check_equal(ints | view::take(10), {0,1,2,3,4,5,6,7,8,9});
-    ::check_equal(ints | view::take(10), {0,1,2,3,4,5,6,7,8,9});
+    {
+        any_view<int> ints = view::ints;
+        ::models<concepts::InputView>(ints);
+        ::models_not<concepts::ForwardView>(ints);
+        ::check_equal(ints | view::take(10), ten_ints);
+        ::check_equal(ints | view::take(10), ten_ints);
+    }
+    {
+        any_view<int> ints2 = view::ints | view::take(10);
+        ::models<concepts::InputView>(ints2);
+        ::models_not<concepts::ForwardView>(ints2);
+        ::check_equal(ints2, ten_ints);
+        ::check_equal(ints2, ten_ints);
+    }
+    {
+        any_random_access_view<int> ints3 = view::ints | view::take(10);
+        ::models<concepts::RandomAccessView>(ints3);
+        ::check_equal(ints3, ten_ints);
+        ::check_equal(ints3, ten_ints);
+        ::check_equal(aux::copy(ints3), ten_ints);
+        ::check_equal(ints3 | view::reverse, {9,8,7,6,5,4,3,2,1,0});
+    }
+    {
+        any_view<int&> e;
+        CHECK(e.begin() == e.begin());
+        CHECK(e.begin() == e.end());
+    }
+    {
+        range_iterator_t<any_random_access_view<int&>> i{},j{};
+        range_sentinel_t<any_random_access_view<int&>> k{};
+        CHECK(i == j);
+        CHECK(i == k);
+        CHECK((i - j) == 0);
+    }
 
-    any_view<int> ints2 = view::ints | view::take(10);
-    ::models<concepts::InputView>(ints2);
-    ::models_not<concepts::ForwardView>(ints2);
-    ::check_equal(ints2, {0,1,2,3,4,5,6,7,8,9});
-    ::check_equal(ints2, {0,1,2,3,4,5,6,7,8,9});
+    // Regression test for #446
+    {
+        auto vec = std::vector<short>{begin(ten_ints), end(ten_ints)};
+        ::check_equal(any_view<int>{vec}, ten_ints);
+        ::check_equal(any_view<int>{ranges::detail::as_const(vec)}, ten_ints);
 
-    any_random_access_view<int> ints3 = view::ints | view::take(10);
-    ::models<concepts::RandomAccessView>(ints3);
-    ::check_equal(ints3, {0,1,2,3,4,5,6,7,8,9});
-    ::check_equal(ints3, {0,1,2,3,4,5,6,7,8,9});
-    ::check_equal(aux::copy(ints3), {0,1,2,3,4,5,6,7,8,9});
-    ::check_equal(ints3 | view::reverse, {9,8,7,6,5,4,3,2,1,0});
+        struct Int {
+            int i_;
 
-    any_view<int&> e;
-    CHECK(e.begin() == e.begin());
-    CHECK(e.begin() == e.end());
-
-    range_iterator_t<any_view<int&>> i{},j{};
-    range_sentinel_t<any_view<int&>> k{};
-    CHECK(i == j);
-    CHECK(i == k);
+            Int(int i) : i_{i} {}
+            operator int() const { return i_; }
+        };
+        auto vec2 = std::vector<Int>{begin(ten_ints), end(ten_ints)};
+        ::check_equal(any_view<int>{vec2}, ten_ints);
+    }
 
     return test_result();
 }
