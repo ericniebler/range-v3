34739693c2f16a082c279466a601d32dc082ac43
Eric Fiselier <eric@efcs.ca>
2016-09-30 02:59:41 -0600

    Refactor CMake URL for better maintainability

--- install_libcxx.sh
+++ install_libcxx.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Install a newer CMake version
-curl -sSL https://cmake.org/files/v3.6/cmake-3.6.1-Linux-x86_64.sh -o install-cmake.sh
+# Update this when LLVM requirements change
+CMAKE_URL = https://cmake.org/files/v3.6/cmake-3.6.1-Linux-x86_64.sh
+curl -sSL $CMAKE_URL -o install-cmake.sh
 chmod +x install-cmake.sh
 sudo ./install-cmake.sh --prefix=/usr/local --skip-license
 
