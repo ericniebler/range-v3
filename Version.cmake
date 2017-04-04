908391303ced5fff6ee267b6042a3c82359f5292
Eric Niebler <eniebler@boost.org>
2017-04-04 08:56:21 -0700

    release notes for 0.2.3

--- Version.cmake
+++ Version.cmake
@@ -2,4 +2,4 @@
 # This makefile will generate a new version.hpp, *AMEND THE MOST RECENT COMMIT*, and git-tag the commit.
 set(RANGE_V3_MAJOR 0)
 set(RANGE_V3_MINOR 2)
-set(RANGE_V3_PATCHLEVEL 2)
+set(RANGE_V3_PATCHLEVEL 3)
