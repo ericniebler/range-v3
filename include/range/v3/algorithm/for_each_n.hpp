3330fef0eea01d4157ef032376c76a9a89bae2c4
Rostislav Khlebnikov <r.khlebnikov@gmail.com>
2017-04-18 14:26:43 +0200

    for_each_n algorithm
    Signed-off-by: Rostislav Khlebnikov <r.khlebnikov@gmail.com>

---
+++ include/range/v3/algorithm/for_each_n.hpp
@@ -0,0 +1,67 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Eric Niebler 2014
+//  Copyright Rostislav Khlebnikov 2017
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+#ifndef RANGES_V3_ALGORITHM_FOR_EACH_N_HPP
+#define RANGES_V3_ALGORITHM_FOR_EACH_N_HPP
+
+#include <functional>
+#include <range/v3/range_fwd.hpp>
+#include <range/v3/range_traits.hpp>
+#include <range/v3/range_concepts.hpp>
+#include <range/v3/algorithm/tagspec.hpp>
+#include <range/v3/utility/functional.hpp>
+#include <range/v3/utility/iterator_traits.hpp>
+#include <range/v3/utility/static_const.hpp>
+#include <range/v3/utility/tagged_pair.hpp>
+
+namespace ranges
+{
+    inline namespace v3
+    {
+        /// \addtogroup group-algorithms
+        /// @{
+        struct for_each_n_fn
+        {
+            template<typename I, typename F, typename P = ident,
+                CONCEPT_REQUIRES_(InputIterator<I>() &&
+                    IndirectInvocable<F, projected<I, P>>())>
+            tagged_pair<tag::in(I), tag::fun(F)>
+            operator()(I begin, difference_type_t<I> n, F fun, P proj = P{}) const
+            {
+                RANGES_EXPECT(0 <= n);
+                auto norig = n;
+                auto b = uncounted(begin);
+                for(; 0 < n; ++b, --n)
+                    invoke(fun, invoke(proj, *b));
+                return {recounted(begin, b, norig), detail::move(fun)};
+            }
+
+            template<typename Rng, typename F, typename P = ident,
+                CONCEPT_REQUIRES_(InputRange<Rng>() &&
+                    IndirectInvocable<F, projected<iterator_t<Rng>, P>>())>
+            tagged_pair<tag::in(safe_iterator_t<Rng>), tag::fun(F)>
+            operator()(Rng &&rng, range_difference_type_t<Rng> n, F fun, P proj = P{}) const
+            {
+                return {(*this)(begin(rng), n, ref(fun), detail::move(proj)).in(),
+                    detail::move(fun)};
+            }
+        };
+
+        /// \sa `for_each_n_fn`
+        /// \ingroup group-algorithms
+        RANGES_INLINE_VARIABLE(with_braced_init_args<for_each_n_fn>, for_each_n)
+        /// @}
+    } // namespace v3
+} // namespace ranges
+
+#endif // include guard
