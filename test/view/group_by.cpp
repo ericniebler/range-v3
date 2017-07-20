98df16ca4651bf833e01d0fe394f3944ebc4b285
Eric Niebler <eniebler@boost.org>
2017-07-20 10:16:41 -0700

    fix view::group_by weirdness

--- test/view/group_by.cpp
+++ test/view/group_by.cpp
@@ -86,5 +86,14 @@ int main()
         check_equal(*rng.begin(), {1, 3, 5});
     }
 
+    {
+        std::vector<int> v {0,1,2,3,4,5,6,7,8,9};
+        auto rng0 = ranges::view::group_by(v, [](int i, int j){ return j - i < 3; });
+        check_equal(*rng0.begin(), {0, 1, 2});
+        check_equal(*next(rng0.begin()), {3, 4, 5});
+        check_equal(*next(rng0.begin(), 2), {6, 7, 8});
+        check_equal(*next(rng0.begin(), 3), {9});
+        CHECK(distance(rng0) == 4);        
+    }
     return test_result();
 }
