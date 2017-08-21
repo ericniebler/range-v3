f0e96406bb007a57e14962b4e3a01e618a170c14
Filip Matzner <FloopCZ@users.noreply.github.com>
2017-08-22 00:07:56 +0200

    Add view::shared (#557)
    * Add view::shared
    
    view::shared was designed to allow the construction of temporary
    containers in the middle of the range pipeline (such as inside
    view::for_each or view::transform). Such temporary containers can be
    afterwards used in the remaining part of the pipeline, without the
    danger of dangling references.
    
    * Rebase view::shared on v0.2.0
    
    * view::shared improvements
    
    - inherit cardinality
    - rename range template arg to Rng
    
    * Fix assertion in view::cycle
    
    * Use the stored rang in view::shared directly
    
    * Improve concept assertions in view::shared
    
    - disable support for sharing a View
    
    * Use detail::decay_t instead of meta::_t<std::decay> in view::shared
    
    * Improve tests of view::shared (by review from @CaseyCarter)
    
    * Simplify concepts in view::shared
    
    * Drop shared_ptr API
    
    * Move view::shared into ranges::experimental
    
    * Remove experimental.hpp header
    
    * Fix spaces in error messages
    
    * Switch namespaces (and indent)
    
    * Improve include guard title

---
+++ include/range/v3/experimental/view/shared.hpp
@@ -0,0 +1,133 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Filip Matzner 2017
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+#ifndef RANGES_V3_EXPERIMENTAL_VIEW_SHARED_HPP
+#define RANGES_V3_EXPERIMENTAL_VIEW_SHARED_HPP
+
+#include <memory>
+#include <type_traits>
+#include <meta/meta.hpp>
+#include <range/v3/begin_end.hpp>
+#include <range/v3/iterator_range.hpp>
+#include <range/v3/range_concepts.hpp>
+#include <range/v3/size.hpp>
+#include <range/v3/view/all.hpp>
+
+namespace ranges
+{
+    inline namespace v3
+    {
+        namespace experimental
+        {
+            template<typename Rng>
+            struct shared_view
+              : view_interface<shared_view<Rng>,
+                range_cardinality<Rng>::value>
+            {
+            private:
+                // shared storage
+                std::shared_ptr<Rng> rng_ptr_;
+
+            public:
+                shared_view() = default;
+
+                // construct from a range rvalue
+                explicit shared_view(Rng && t)
+                  : rng_ptr_{std::make_shared<Rng>(std::move(t))}
+                {}
+
+                // use the stored range's begin and end
+                iterator_t<Rng> begin() const
+                {
+                    return ranges::begin(*rng_ptr_);
+                }
+                sentinel_t<Rng> end() const
+                {
+                    return ranges::end(*rng_ptr_);
+                }
+
+                // use the const-most size() function provided by the range
+                CONCEPT_REQUIRES(SizedRange<const Rng>())
+                range_size_type_t<Rng> size() const
+                {
+                    return ranges::size(*rng_ptr_);
+                }
+                CONCEPT_REQUIRES(SizedRange<Rng>() && !SizedRange<const Rng>())
+                range_size_type_t<Rng> size()
+                {
+                    return ranges::size(*rng_ptr_);
+                }
+            };
+
+            /// \relates all
+            /// \addtogroup group-views
+            /// @{
+            namespace view
+            {
+                struct shared_fn : pipeable<shared_fn>
+                {
+                public:
+#ifndef RANGES_DOXYGEN_INVOKED
+                    template<typename Rng,
+                        CONCEPT_REQUIRES_(!Range<Rng>())>
+                    void operator()(std::shared_ptr<Rng>) const
+                    {
+                        CONCEPT_ASSERT_MSG(Range<Rng>(),
+                            "The object on which view::shared operates must be a "
+                            "model of the Range concept.");
+                    }
+#endif
+
+                    template<typename Rng,
+                        CONCEPT_REQUIRES_(Range<Rng>()
+                                          && !View<Rng>()
+                                          && !std::is_reference<Rng>::value)>
+                    shared_view<Rng> operator()(Rng && t) const
+                    {
+                        return shared_view<Rng>{std::move(t)};
+                    }
+
+#ifndef RANGES_DOXYGEN_INVOKED
+                    template<typename Rng,
+                        CONCEPT_REQUIRES_(!Range<Rng>()
+                                          || View<Rng>()
+                                          || std::is_reference<Rng>::value)>
+                    void operator()(Rng &&) const
+                    {
+                        CONCEPT_ASSERT_MSG(Range<Rng>(),
+                            "The object on which view::shared operates must be a "
+                            "model of the Range concept.");
+                        CONCEPT_ASSERT_MSG(!View<Rng>(),
+                            "view::shared cannot be constructed from a view. "
+                            "Please copy the original view instead.");
+                        CONCEPT_ASSERT_MSG(!std::is_reference<Rng>::value,
+                            "view::shared needs an rvalue reference "
+                            "to build a shared object.");
+                    }
+#endif
+                };
+
+                /// \relates all_fn
+                /// \ingroup group-views
+                RANGES_INLINE_VARIABLE(shared_fn, shared)
+
+                template<typename Rng>
+                using shared_t =
+                    detail::decay_t<decltype(shared(std::declval<Rng>()))>;
+
+            } // namespace view
+            /// @}
+        } // namespace experimental
+    } // namespace v3
+} // namespace ranges
+
+#endif // include guard
---
+++ test/view/shared.cpp
@@ -0,0 +1,144 @@
+// Range v3 library
+//
+//  Copyright Filip Matzner 2017
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+
+#include <list>
+#include <memory>
+#include <tuple>
+#include <vector>
+#include <range/v3/core.hpp>
+#include <range/v3/algorithm/for_each.hpp>
+#include <range/v3/view/cycle.hpp>
+#include <range/v3/view/for_each.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/join.hpp>
+#include <range/v3/view/remove_if.hpp>
+#include <range/v3/view/repeat.hpp>
+#include <range/v3/view/reverse.hpp>
+#include <range/v3/experimental/view/shared.hpp>
+#include <range/v3/view/take.hpp>
+#include "../simple_test.hpp"
+#include "../test_utils.hpp"
+
+using namespace ranges;
+
+template<typename T>
+void check_shared_contents()
+{
+    // build two instances sharing the same range
+    experimental::shared_view<T> view1 = experimental::view::shared(T{1, 1, 1, 2, 3, 4, 4});
+    experimental::shared_view<T> view2 = view1;
+
+    // check the length of the views
+    CHECK(view1.size() == 7u);
+    CHECK(view2.size() == 7u);
+
+    // check the stored numbers
+    auto check_values = [](experimental::shared_view<T> & rng) {
+      ::check_equal(view::cycle(rng) | view::take(10), {1, 1, 1, 2, 3, 4, 4, 1, 1, 1});
+      ::check_equal(view::all(rng) | view::take(5), {1, 1, 1, 2, 3});
+      ::check_equal(rng | view::take(5), {1, 1, 1, 2, 3});
+      ::check_equal(rng, {1, 1, 1, 2, 3, 4, 4});
+    };
+    check_values(view1);
+    check_values(view2);
+
+    // check that changes are shared
+    *(++begin(view1)) = 7;
+    CHECK(*(++begin(view2)) == 7);
+    *begin(view2) = 3;
+    CHECK(*begin(view1) == 3);
+}
+
+int main()
+{
+    // check shared random access range
+    check_shared_contents<std::vector<int>>();
+    // check shared bidirectional range
+    check_shared_contents<std::list<int>>();
+
+    {
+        // check the piped construction from an rvalue
+        std::vector<int> base_vec = {1, 2, 2, 8, 2, 7};
+        auto vec_view = std::move(base_vec) | experimental::view::shared;
+        CHECK(vec_view.size() == 6u);
+        ::check_equal(vec_view, {1, 2, 2, 8, 2, 7});
+    }
+
+    {
+        // test bidirectional range
+        auto list_view = std::list<int>{1, 2, 3} | experimental::view::shared;
+
+        CHECK(list_view.size() == 3u);
+        has_type<int &>(*begin(list_view));
+        ::models<concepts::SizedRange>(list_view);
+        ::models<concepts::BoundedRange>(list_view);
+        ::models<concepts::BidirectionalRange>(list_view);
+        ::models_not<concepts::RandomAccessRange>(list_view);
+
+        // test bidirectional range iterator
+        CHECK(*begin(list_view) == 1);
+        CHECK(*prev(end(list_view)) == 3);
+    }
+
+    {
+        // test random access range
+        auto vec_view = std::vector<int>{1, 2, 3} | experimental::view::shared;
+
+        CHECK(vec_view.size() == 3u);
+        has_type<int &>(*begin(vec_view));
+        ::models<concepts::SizedRange>(vec_view);
+        ::models<concepts::BoundedRange>(vec_view);
+        ::models<concepts::RandomAccessRange>(vec_view);
+        CHECK(vec_view[0] == 1);
+        CHECK(vec_view[1] == 2);
+        CHECK(vec_view[2] == 3);
+    }
+
+    {
+        // check temporary value in view::transform
+        auto f = [](unsigned a){ return std::vector<unsigned>(a, a); };
+
+        auto vec_view =
+            view::iota(1u)
+          | view::transform(f)
+          | view::transform(experimental::view::shared)
+          | view::join
+          | view::take(10);
+
+        ::check_equal(vec_view, {1u, 2u, 2u, 3u, 3u, 3u, 4u, 4u, 4u, 4u});
+    }
+
+    {
+        // check temporary value in view::for_each
+        std::vector<int> base_vec{1, 2, 3};
+        auto vec_view =
+            view::repeat(base_vec)
+          | view::for_each([](std::vector<int> tmp) {
+                return yield_from(std::move(tmp) | experimental::view::shared | view::reverse);
+            })
+          | view::take(7);
+        ::check_equal(vec_view, {3, 2, 1, 3, 2, 1, 3});
+    }
+
+    {
+        // check temporary value in view::for_each without the yield_from
+        std::vector<int> base_vec{1, 2, 3};
+        auto vec_view =
+            view::repeat(base_vec)
+          | view::for_each([](std::vector<int> tmp) {
+                return std::move(tmp) | experimental::view::shared | view::reverse;
+            })
+          | view::take(7);
+        ::check_equal(vec_view, {3, 2, 1, 3, 2, 1, 3});
+    }
+
+    return test_result();
+}
