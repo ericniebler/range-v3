bce993fc209bc59194a833c1609e1ede15697216
Casey Carter <Casey@Carter.net>
2017-10-14 09:40:41 -0700

    stride_view refactor:
    stride views must track the distance in the base range between the last dereferenceable iterator and the end iterator ("offset"). The offset is used when decrementing bidirectional end iterators. Since there's one unique offset value per underlying range, we can store a single offset value in the stride_view rather than in each of its iterators.
    
    Using a mutable std::atomic to store the offset and provide const-iterable stride_views was a design mistake: all users pay the cost of the atomic despite that few users will actually access the same stride_view simultaneously from multiple threads. Let's instead store the offset in a plain vanilla member variable, and make the view not const iterable when it must update the offset during iteration.

--- test/view/stride.cpp
+++ test/view/stride.cpp
@@ -18,7 +18,6 @@
 #include <range/v3/view/reverse.hpp>
 #include <range/v3/view/stride.hpp>
 #include <range/v3/algorithm/copy.hpp>
-#include <range/v3/utility/counted_iterator.hpp>
 #include <range/v3/utility/iterator.hpp>
 #include <range/v3/numeric.hpp>
 #include "../simple_test.hpp"
@@ -29,44 +28,68 @@ int main()
 {
     using namespace ranges;
 
-    std::vector<int> v(50);
-    iota(v, 0);
+    auto const v = []
+    {
+        std::vector<int> v(50);
+        iota(v, 0);
+        return v;
+    }();
+    {
+        auto rng = v | view::stride(3);
+        using R = decltype(rng);
+        CONCEPT_ASSERT(RandomAccessView<R>());
+        CONCEPT_ASSERT(!ContiguousRange<R>());
+        CONCEPT_ASSERT(BoundedRange<R>());
+        CONCEPT_ASSERT(SizedRange<R>());
+        CONCEPT_ASSERT(Range<R const>());
+        ::check_equal(rng | view::reverse,
+                    {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
+    }
 
-    if (!ranges::v3::detail::broken_ebo)
-        CHECK(
-            sizeof((v | view::stride(3)).begin()) ==
-            sizeof(void*) + sizeof(v.begin()) + sizeof(std::ptrdiff_t));
-    ::check_equal(v | view::stride(3) | view::reverse,
-                  {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
+    {
+        std::stringstream str;
+        copy(v, ostream_iterator<int>{str, " "});
+        auto rng = istream<int>(str) | view::stride(3);
+        using R = decltype(rng);
+        CONCEPT_ASSERT(InputView<R>());
+        CONCEPT_ASSERT(!ForwardRange<R>());
+        CONCEPT_ASSERT(!BoundedRange<R>());
+        CONCEPT_ASSERT(!SizedRange<R>());
+        CONCEPT_ASSERT(!Range<R const>());
+        check_equal(rng, {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
+    }
 
-    std::stringstream str;
-    copy(v, ostream_iterator<int>{str, " "});
-    auto x = istream<int>(str) | view::stride(3);
-    CONCEPT_ASSERT(InputView<decltype(x)>());
-    CONCEPT_ASSERT(!InputView<decltype(x) const>());
-    CONCEPT_ASSERT(!ForwardView<decltype(x)>());
-    check_equal(x, {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
+    {
+        std::list<int> li;
+        copy(v, back_inserter(li));
+        auto rng = li | view::stride(3);
+        using R = decltype(rng);
+        CONCEPT_ASSERT(BidirectionalView<R>());
+        CONCEPT_ASSERT(!RandomAccessRange<R>());
+        CONCEPT_ASSERT(BoundedRange<R>());
+        CONCEPT_ASSERT(SizedRange<R>());
+        CONCEPT_ASSERT(Range<R const>());
+        ::check_equal(rng,
+                    {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
+        ::check_equal(rng | view::reverse,
+                    {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
 
-    std::list<int> li;
-    copy(v, back_inserter(li));
-    ::check_equal(li | view::stride(3),
-                  {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
-    ::check_equal(li | view::stride(3) | view::reverse,
-                  {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
+        for(int i : rng | view::reverse)
+            std::cout << i << ' ';
+        std::cout << '\n';
+    }
 
-    for(int i : li | view::stride(3) | view::reverse)
-        std::cout << i << ' ';
-    std::cout << '\n';
+    {
+        auto x2 = v | view::stride(3);
+        CHECK(ranges::distance(x2) == 17);
 
-    auto x2 = v | view::stride(3);
-    CHECK(ranges::distance(x2) == 17);
-
-    auto it0 = x2.begin();
-    auto it1 = std::next(it0, 10);
-    CHECK((it1 - it0) == 10);
-    CHECK((it0 - it1) == -10);
-    CHECK((it0 - it0) == 0);
-    CHECK((it1 - it1) == 0);
+        auto it0 = x2.begin();
+        auto it1 = std::next(it0, 10);
+        CHECK((it1 - it0) == 10);
+        CHECK((it0 - it1) == -10);
+        CHECK((it0 - it0) == 0);
+        CHECK((it1 - it1) == 0);
+    }
 
     {
         const auto n = 4;
@@ -83,6 +106,12 @@ int main()
     {
         int const some_ints[] = {0,1,2,3,4,5,6,7};
         auto rng = debug_input_view<int const>{some_ints} | view::stride(2);
+        using R = decltype(rng);
+        CONCEPT_ASSERT(InputView<R>());
+        CONCEPT_ASSERT(!ForwardRange<R>());
+        CONCEPT_ASSERT(!BoundedRange<R>());
+        CONCEPT_ASSERT(SizedRange<R>());
+        CONCEPT_ASSERT(!Range<R const>());
         ::check_equal(rng, {0,2,4,6});
     }
 
