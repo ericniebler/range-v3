d7d19a9c7add90d22c70d9cb6fa793d802979058
Paul le Roux <pleroux0@outlook.com>
2018-06-19 10:50:33 +0200

    Improve conan integration by using cmake install

--- conanfile.py
+++ conanfile.py
@@ -10,16 +10,29 @@
 # Project home: https://github.com/ericniebler/range-v3
 #
 
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 class Rangev3Conan(ConanFile):
     name = "range-v3"
     license = "Boost Software License - Version 1.0 - August 17th, 2003"
     url = "https://github.com/ericniebler/range-v3"
     description = """Experimental range library for C++11/14/17"""
-    exports_sources = "include*", "LICENSE.txt"
+    exports_sources = "include*", "LICENSE.txt", "CMakeLists.txt", "cmake/*", "Version.cmake", "version.hpp.in"
     build_policy = "missing"
 
+    def build(self):
+        pass
+
     def package(self):
-        self.copy("*.hpp", src="include", dst="include", keep_path=True)
+        cmake = CMake(self)
+        cmake.definitions["BUILD_TESTING"] = "OFF"
+        cmake.definitions["RANGE_V3_EXAMPLES"] = "OFF"
+        cmake.definitions["RANGE_V3_PERF"] = "OFF"
+        cmake.definitions["RANGE_V3_DOCS"] = "OFF"
+        cmake.configure()
+        cmake.install()
+
         self.copy("LICENSE.txt", dst="licenses", ignore_case=True, keep_path=False)
+
+    def package_info(self):
+        self.info.header_only()
