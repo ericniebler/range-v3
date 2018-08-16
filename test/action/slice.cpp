8676d9b1ccf7c85e0e4a6a22159d7f192b70c3d6
Miguel Sacristan <miguel_tete17@hotmail.com>
2018-08-16 21:20:24 +0200

    Rewrite unit tests for action::slice

--- test/action/slice.cpp
+++ test/action/slice.cpp
@@ -23,22 +23,34 @@ int main()
 {
     using namespace ranges;
 
-    std::vector<int> v = view::ints(0,100);
+    std::vector<int> rng = view::ints(0,100);
 
-    auto v2 = v | copy | action::slice(10,20);
-    CHECK(size(v2) == 10u);
-    ::models<concepts::Same>(v, v2);
-    ::check_equal(v2, {10,11,12,13,14,15,16,17,18,19});
+    auto rng_copy = rng | copy | action::slice(10,20);
+    CHECK(size(rng_copy) == 10u);
+    ::models<concepts::Same>(rng, rng_copy);
+    ::check_equal(rng_copy, {10,11,12,13,14,15,16,17,18,19});
 
-    v2 = v2 | move | action::slice(2,8);
-    ::check_equal(v2, {12,13,14,15,16,17});
+    std::vector<int> rng_2 = view::ints(0,100);
 
-    v2 |= action::slice(0,0);
-    CHECK(v2.size() == 0u);
+    rng_2 |= action::slice(20,end-70);
+    CHECK(size(rng_2) == 10u);
+    ::check_equal(rng_2, {20,21,22,23,24,25,26,27,28,29});
 
-    auto & v3 = action::slice(v, 90, 100);
-    CHECK(&v3 == &v);
-    ::check_equal(v, {90,91,92,93,94,95,96,97,98,99});
+    rng_2 |= action::slice(end-10,end-5);
+    CHECK(size(rng_2) == 5u);
+    ::check_equal(rng_2, {20,21,22,23,24});
+
+    std::vector<int> rng_3 = view::ints(0,100);
+
+    auto& rng_3_copy = action::slice(rng_3, 90, end);
+    CHECK(&rng_3_copy == &rng_3);
+    CHECK(size(rng_3_copy) == 10u);
+    ::check_equal(rng_3, {90,91,92,93,94,95,96,97,98,99});
+
+    rng_3 |= action::slice(end-5, end);
+    CHECK(&rng_3_copy == &rng_3);
+    CHECK(size(rng_3_copy) == 5u);
+    ::check_equal(rng_3, {95,96,97,98,99});
 
     return ::test_result();
 }
