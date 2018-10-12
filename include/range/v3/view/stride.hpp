dd3f016f7c525723775a1dfe1c2d2e1e69723037
Eric Niebler <eniebler@boost.org>
2018-10-12 09:41:17 -0700

    fix bug in stride_view's iterator advance; fixes #901 (#907)
    fix bug in stride_view's iterator advance; fixes #901

--- include/range/v3/view/stride.hpp
+++ include/range/v3/view/stride.hpp
@@ -59,8 +59,6 @@ namespace ranges
                 stride_view_base() = default;
                 RANGES_CXX14_CONSTEXPR
                 stride_view_base(Rng &&rng, range_difference_type_t<Rng> const stride)
-                    noexcept(std::is_nothrow_constructible<stride_view_adaptor<Rng>, Rng>::value &&
-                        noexcept(std::declval<stride_view_base &>().calc_offset(SizedRange<Rng>())))
                   : stride_view_adaptor<Rng>{std::move(rng)},
                     stride_{(RANGES_EXPECT(0 < stride), stride)},
                     offset_{calc_offset(SizedRange<Rng>())}
@@ -88,7 +86,6 @@ namespace ranges
             private:
                 RANGES_CXX14_CONSTEXPR
                 range_difference_type_t<Rng> calc_offset(std::true_type)
-                    noexcept(noexcept(ranges::distance(std::declval<stride_view_base &>().base())))
                 {
                     if(auto const rem = ranges::distance(this->base()) % stride_)
                         return stride_ - rem;
@@ -108,7 +105,6 @@ namespace ranges
             {
                 stride_view_base() = default;
                 constexpr stride_view_base(Rng &&rng, range_difference_type_t<Rng> const stride)
-                    noexcept(std::is_nothrow_constructible<stride_view_adaptor<Rng>, Rng>::value)
                   : stride_view_adaptor<Rng>{std::move(rng)},
                     stride_{(RANGES_EXPECT(0 < stride), stride)}
                 {}
@@ -142,6 +138,14 @@ namespace ranges
             static constexpr bool const_iterable =
                 Range<Rng const>() && (SizedRange<Rng>() || !BidirectionalRange<Rng>());
 
+            // If the underlying range doesn't model BoundedRange, then we can't
+            // decrement the end and there's no reason to adapt the sentinel. Strictly
+            // speaking, we don't have to adapt the end iterator of Input and Forward
+            // Ranges, but in the interests of making the resulting stride view model
+            // BoundedView, adapt it anyway.
+            static constexpr bool can_bound = BoundedRange<Rng>()
+                && (SizedRange<Rng>() || !BidirectionalRange<Rng>());
+
             struct adaptor : adaptor_base
             {
             private:
@@ -153,8 +157,6 @@ namespace ranges
                   : rng_(&rng)
                 {}
                 RANGES_CXX14_CONSTEXPR void next(iterator_t<Rng> &it)
-                    noexcept(noexcept(it != ranges::end(std::declval<Rng &>()),
-                        ranges::advance(it, 0, std::declval<sentinel_t<Rng> &>())))
                 {
                     auto const last = ranges::end(rng_->base());
                     RANGES_EXPECT(it != last);
@@ -166,19 +168,12 @@ namespace ranges
                 }
                 CONCEPT_REQUIRES(BidirectionalRange<Rng>())
                 RANGES_CXX14_CONSTEXPR void prev(iterator_t<Rng> &it)
-                    noexcept(noexcept(ranges::advance(it, 0),
-                        it != ranges::begin(std::declval<Rng &>()),
-                        it == ranges::end(std::declval<Rng &>())))
                 {
                     RANGES_EXPECT(it != ranges::begin(rng_->base()));
                     auto delta = -rng_->stride_;
                     if(it == ranges::end(rng_->base()))
                     {
-                        if(rng_->get_offset(false) < 0) // hasn't been set yet!
-                        {
-                            auto const rem = ranges::distance(rng_->base()) % rng_->stride_;
-                            rng_->set_offset(rem ? rng_->stride_ - rem : 0);
-                        }
+                        RANGES_EXPECT(rng_->get_offset() >= 0);
                         delta += rng_->get_offset();
                     }
                     ranges::advance(it, delta);
@@ -187,7 +182,6 @@ namespace ranges
                     CONCEPT_REQUIRES_(SizedSentinel<Other, iterator_t<Rng>>())>
                 RANGES_CXX14_CONSTEXPR range_difference_type_t<Rng> distance_to(
                     iterator_t<Rng> const &here, Other const &there) const
-                    noexcept(noexcept(there - here))
                 {
                     range_difference_type_t<Rng> delta = there - here;
                     if(delta < 0)
@@ -199,16 +193,17 @@ namespace ranges
                 CONCEPT_REQUIRES(RandomAccessRange<Rng>())
                 RANGES_CXX14_CONSTEXPR void advance(
                     iterator_t<Rng> &it, range_difference_type_t<Rng> n)
-                    noexcept(noexcept(
-                        ranges::begin(std::declval<Rng &>()) == ranges::end(std::declval<Rng &>()),
-                        ranges::advance(it, n, std::declval<sentinel_t<Rng> &>()),
-                        ranges::advance(it, n),
-                        ranges::advance(it, n, std::declval<iterator_t<Rng> &>())))
                 {
-                    if(0 == n) return;
+                    if(0 == n)
+                        return;
                     n *= rng_->stride_;
                     auto const last = ranges::end(rng_->base());
-                    if(it == last) n -= rng_->get_offset();
+                    if(it == last)
+                    {
+                        RANGES_EXPECT(n < 0);
+                        RANGES_EXPECT(rng_->get_offset() >= 0);
+                        n += rng_->get_offset();
+                    }
                     if(0 < n)
                     {
                         auto delta = ranges::advance(it, n, last);
@@ -232,46 +227,32 @@ namespace ranges
             };
             CONCEPT_REQUIRES(const_iterable)
             constexpr adaptor begin_adaptor() const
-                noexcept(std::is_nothrow_constructible<adaptor, stride_view const &>::value &&
-                    std::is_nothrow_move_constructible<adaptor>::value)
             {
                 return adaptor{*this};
             }
             CONCEPT_REQUIRES(!const_iterable)
             RANGES_CXX14_CONSTEXPR adaptor begin_adaptor()
-                noexcept(std::is_nothrow_constructible<adaptor, stride_view &>::value &&
-                    std::is_nothrow_move_constructible<adaptor>::value)
             {
                 return adaptor{*this};
             }
-            // If the underlying sequence object doesn't model BoundedRange, then we can't
-            // decrement the end and there's no reason to adapt the sentinel. Strictly
-            // speaking, we don't have to adapt the end iterator of Input and Forward
-            // Ranges, but in the interests of making the resulting stride view model
-            // BoundedView, adapt it anyway.
-            CONCEPT_REQUIRES(const_iterable && BoundedRange<Rng>())
+
+            CONCEPT_REQUIRES(const_iterable && can_bound)
             constexpr adaptor end_adaptor() const
-                noexcept(std::is_nothrow_constructible<adaptor, stride_view const &>::value &&
-                    std::is_nothrow_move_constructible<adaptor>::value)
             {
                 return adaptor{*this};
             }
-            CONCEPT_REQUIRES(!const_iterable && BoundedRange<Rng>())
+            CONCEPT_REQUIRES(!const_iterable && can_bound)
             RANGES_CXX14_CONSTEXPR adaptor end_adaptor()
-                noexcept(std::is_nothrow_constructible<adaptor, stride_view &>::value &&
-                    std::is_nothrow_move_constructible<adaptor>::value)
             {
                 return adaptor{*this};
             }
-            CONCEPT_REQUIRES(const_iterable && !BoundedRange<Rng>())
+            CONCEPT_REQUIRES(const_iterable && !can_bound)
             constexpr adaptor_base end_adaptor() const
-                noexcept(std::is_nothrow_constructible<adaptor_base, stride_view const &>::value)
             {
                 return {};
             }
-            CONCEPT_REQUIRES(!const_iterable && !BoundedRange<Rng>())
+            CONCEPT_REQUIRES(!const_iterable && !can_bound)
             RANGES_CXX14_CONSTEXPR adaptor_base end_adaptor()
-                noexcept(std::is_nothrow_constructible<adaptor_base, stride_view &>::value)
             {
                 return {};
             }
@@ -284,19 +265,15 @@ namespace ranges
         public:
             stride_view() = default;
             constexpr stride_view(Rng rng, range_difference_type_t<Rng> const stride)
-                noexcept(std::is_nothrow_constructible<detail::stride_view_base<Rng>,
-                    Rng, range_difference_type_t<Rng>>::value)
               : detail::stride_view_base<Rng>{std::move(rng), stride}
             {}
             CONCEPT_REQUIRES(SizedRange<Rng const>())
             constexpr range_size_type_t<Rng> size() const
-                noexcept(noexcept(ranges::size(std::declval<Rng const &>())))
             {
                 return size_(ranges::size(this->base()));
             }
             CONCEPT_REQUIRES(!SizedRange<Rng const>() && SizedRange<Rng>())
             RANGES_CXX14_CONSTEXPR range_size_type_t<Rng> size()
-                noexcept(noexcept(ranges::size(std::declval<Rng &>())))
             {
                 return size_(ranges::size(this->base()));
             }
--- test/view/stride.cpp
+++ test/view/stride.cpp
@@ -17,6 +17,7 @@
 #include <range/v3/view/move.hpp>
 #include <range/v3/view/reverse.hpp>
 #include <range/v3/view/stride.hpp>
+#include <range/v3/view/iota.hpp>
 #include <range/v3/algorithm/copy.hpp>
 #include <range/v3/utility/iterator.hpp>
 #include <range/v3/numeric.hpp>
@@ -123,9 +124,26 @@ int main()
         using R = decltype(rng);
         CONCEPT_ASSERT(BidirectionalView<R>());
         CONCEPT_ASSERT(!RandomAccessRange<R>());
-        CONCEPT_ASSERT(BoundedRange<R>());
+        CONCEPT_ASSERT(!BoundedRange<R>());
         CONCEPT_ASSERT(!SizedRange<R>());
         CONCEPT_ASSERT(!Range<R const>());
+        ::check_equal(rng,
+                    {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
+        ::check_equal(rng | view::reverse,
+                    {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
+    }
+
+    {
+        std::list<int> li;
+        copy(v, back_inserter(li));
+        sized_iterator_range<std::list<int>::const_iterator> tmp{li.begin(), li.end(), li.size()};
+        auto rng = tmp | view::stride(3);
+        using R = decltype(rng);
+        CONCEPT_ASSERT(BidirectionalView<R>());
+        CONCEPT_ASSERT(!RandomAccessRange<R>());
+        CONCEPT_ASSERT(BoundedRange<R>());
+        CONCEPT_ASSERT(SizedRange<R>());
+        CONCEPT_ASSERT(Range<R const>());
         CHECK((*--rng.end()) == 48);
         ::check_equal(rng,
                     {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48});
@@ -133,5 +151,30 @@ int main()
                     {48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0});
     }
 
-    return ::test_result();
+    // https://github.com/ericniebler/range-v3/issues/901
+    {
+        auto r = view::iota( 0, 12 );
+
+        // Evenly divisible stride:
+        auto strided1 = r | view::stride(3);
+        ::check_equal(strided1, {0, 3, 6, 9});
+        CHECK(strided1.size() == 4u);
+        CHECK(strided1.front() == 0);
+        CHECK(strided1[0] == 0);
+        CHECK(strided1.back() == 9);
+        CHECK(strided1[3] == 9);
+        CHECK(strided1[(int)strided1.size() - 1] == 9);
+
+        // Not evenly divisible stride:
+        auto strided2 = r | view::stride(5);
+        ::check_equal(strided2, {0, 5, 10});
+        CHECK(strided2.size() == 3u);
+        CHECK(strided2.front() == 0);
+        CHECK(strided2[0] == 0);
+        CHECK(strided2.back() == 10);
+        CHECK(strided2[2] == 10);
+        CHECK(strided2[(int)strided2.size() - 1] == 10);
+    }
+
+    //return ::test_result();
 }
