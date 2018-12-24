21b81b9d68c574fe1f2f0fcc6f83af0cb7cd063f
Ludger Sprenker <ludger@sprenker.net>
2018-12-24 15:43:53 +0100

    Projection support for view::drop_while

--- include/range/v3/view/drop_while.hpp
+++ include/range/v3/view/drop_while.hpp
@@ -87,11 +87,19 @@ namespace ranges
                 (
                     make_pipeable(std::bind(drop_while, std::placeholders::_1, protect(std::move(pred))))
                 )
+
+                template<typename Pred, typename Proj>
+                static auto bind(drop_while_fn drop_while, Pred pred, Proj proj)
+                RANGES_DECLTYPE_AUTO_RETURN
+                (
+                    make_pipeable(std::bind(drop_while, std::placeholders::_1, 
+                        protect(std::move(pred)), protect(std::move(proj))))
+                )
             public:
-                template<typename Rng, typename Pred>
+                template<typename Rng, typename Pred, typename Proj = ident>
                 using Concept = meta::and_<
                     InputRange<Rng>,
-                    IndirectPredicate<Pred, iterator_t<Rng>>>;
+                    IndirectPredicate<Pred, projected<iterator_t<Rng>, Proj>>>;
 
                 template<typename Rng, typename Pred,
                     CONCEPT_REQUIRES_(Concept<Rng, Pred>())>
@@ -100,6 +108,18 @@ namespace ranges
                 {
                     return {all(static_cast<Rng&&>(rng)), std::move(pred)};
                 }
+
+                template<typename Rng, typename Pred, typename Proj,
+                    CONCEPT_REQUIRES_(Concept<Rng, Pred, Proj>())>
+                RANGES_CXX14_CONSTEXPR
+                auto operator()(Rng && rng, Pred pred, Proj proj) const
+                RANGES_DECLTYPE_AUTO_RETURN_NOEXCEPT
+                (
+                    drop_while_view<all_t<Rng>, composed<Pred, Proj>>{
+                        all(static_cast<Rng&&>(rng)),
+                        compose(std::move(pred), std::move(proj))
+                    }
+                )
             #ifndef RANGES_DOXYGEN_INVOKED
                 template<typename Rng, typename Pred,
                     CONCEPT_REQUIRES_(!Concept<Rng, Pred>())>
@@ -112,6 +132,22 @@ namespace ranges
                         "The second argument to view::drop_while must be callable with "
                         "an argument of the range's common reference type, and its return value "
                         "must be convertible to bool");
+                }
+
+                template<typename Rng, typename Pred, typename Proj,
+                    CONCEPT_REQUIRES_(!Concept<Rng, Pred, Proj>())>
+                void operator()(Rng &&, Pred, Proj) const
+                {
+                    CONCEPT_ASSERT_MSG(InputRange<Rng>(),
+                        "The first argument to view::drop_while must be a model of the "
+                        "InputRange concept");
+                    using Itr = iterator_t<Rng>;
+                    CONCEPT_ASSERT_MSG(IndirectInvocable<Proj, Itr>(),
+                        "The projection function must accept arguments of the iterator's "
+                        "value type, reference type, and common reference type.");
+                    CONCEPT_ASSERT_MSG(IndirectPredicate<Pred, projected<Itr, Proj>>(),
+                        "The second argument to view::drop_while must accept "
+                        "values returned by the projection function.");
                 }
             #endif
             };
--- test/view/drop_while.cpp
+++ test/view/drop_while.cpp
@@ -18,6 +18,16 @@
 #include "../simple_test.hpp"
 #include "../test_utils.hpp"
 
+struct my_data
+{
+    int i;
+};
+
+bool operator==(my_data left, my_data right)
+{
+    return left.i == right.i;
+}
+
 int main()
 {
     using namespace ranges;
@@ -57,6 +67,13 @@ int main()
         CONCEPT_ASSERT(!BoundedRange<R>());
         CONCEPT_ASSERT(Same<int const&, range_reference_t<R>>());
         ::check_equal(rng, {4,5,6,7,8,9});
+    }
+
+    {
+        // with projection
+        const std::list<my_data> data_list{{1}, {2}, {3}, {1}};
+        auto rng = data_list | view::drop_while([](int i){ return i <= 2; }, &my_data::i);
+        ::check_equal(rng, std::list<my_data>{{3}, {1}});
     }
 
     return test_result();
