0c96cf41be788678bbc174e74bc722572624f00c
Eric Niebler <eniebler@boost.org>
2019-01-10 23:16:37 -0800

    take_view conforms to WD

--- include/range/v3/view/take.hpp
+++ include/range/v3/view/take.hpp
@@ -18,89 +18,236 @@
 #include <range/v3/range_fwd.hpp>
 #include <range/v3/range_concepts.hpp>
 #include <range/v3/range_traits.hpp>
-#include <range/v3/view_adaptor.hpp>
 #include <range/v3/algorithm/min.hpp>
 #include <range/v3/detail/satisfy_boost_range.hpp>
 #include <range/v3/utility/counted_iterator.hpp>
 #include <range/v3/utility/functional.hpp>
 #include <range/v3/utility/static_const.hpp>
-#include <range/v3/view/take_exactly.hpp>
 #include <range/v3/view/view.hpp>
+#include <range/v3/view/all.hpp>
 
 namespace ranges
 {
     template<typename Rng>
     struct take_view
-      : view_adaptor<take_view<Rng>, Rng, finite>
+      : view_interface<take_view<Rng>>
     {
     private:
-        friend range_access;
-
-        range_difference_t<Rng> n_ = 0;
-
-        template<bool IsConst>
-        using CI = counted_iterator<iterator_t<meta::const_if_c<IsConst, Rng>>>;
-        template<bool IsConst>
-        using S = sentinel_t<meta::const_if_c<IsConst, Rng>>;
-
-        template<bool IsConst>
-        struct adaptor : adaptor_base
+        CPP_assert(View<Rng>);
+        Rng base_ = Rng();
+        range_difference_t<Rng> count_ = 0;
+        template<bool Const>
+        struct sentinel
         {
-            adaptor() = default;
-            template<bool Other>
-            CPP_ctor(adaptor)(adaptor<Other>)(
-                requires IsConst && (!Other))
+        private:
+            using Base = detail::if_then_t<Const, Rng const, Rng>;
+            using CI = counted_iterator<iterator_t<Base>>;
+            sentinel_t<Base> end_ = sentinel_t<Base>();
+        public:
+            sentinel() = default;
+            constexpr explicit sentinel(sentinel_t<Base> end)
+              : end_(std::move(end))
             {}
-            CI<IsConst> begin(meta::const_if_c<IsConst, take_view> &rng) const
+            CPP_member
+            constexpr CPP_ctor(sentinel)(sentinel<!Const> that)(
+                requires Const && ConvertibleTo<sentinel_t<Rng>, sentinel_t<Base>>)
+              : end_(std::move(that.end_))
+            {}
+            constexpr sentinel_t<Base> base() const
             {
-                return {ranges::begin(rng.base()), rng.n_};
+                return end_;
+            }
+            friend constexpr bool operator==(sentinel const &x, CI const &y)
+            {
+                return y.count() == 0 || y.base() == x.end_;
+            }
+            friend constexpr bool operator==(CI const &y, sentinel const &x)
+            {
+                return y.count() == 0 || y.base() == x.end_;
+            }
+            friend constexpr bool operator!=(sentinel const &x, CI const &y)
+            {
+                return y.count() != 0 && y.base() != x.end_;
+            }
+            friend constexpr bool operator!=(CI const &y, sentinel const &x)
+            {
+                return y.count() != 0 && y.base() != x.end_;
             }
         };
 
-        template<bool IsConst>
-        struct sentinel_adaptor : adaptor_base
+#if RANGES_CXX_IF_CONSTEXPR < RANGES_CXX_IF_CONSTEXPR_17
+        template<typename Take>
+        static auto begin_random_access_(Take &take, std::true_type)
         {
-            sentinel_adaptor() = default;
-            template<bool Other>
-            CPP_ctor(sentinel_adaptor)(sentinel_adaptor<Other>)(
-                requires IsConst && (!Other))
-            {}
-            bool empty(CI<IsConst> const &that, S<IsConst> const &sent) const
-            {
-                return 0 == that.count() || sent == that.base();
-            }
-        };
+            return ranges::begin(take.base_);
+        }
+        template<typename Take>
+        static auto begin_random_access_(Take &take, std::false_type)
+        {
+            auto s = static_cast<range_difference_t<Rng>>(take.size());
+            return make_counted_iterator(ranges::begin(take.base_), s);
+        }
+        template<typename Take>
+        static auto begin_sized_(Take &take, std::true_type)
+        {
+            return begin_random_access_(
+                take,
+                meta::bool_<RandomAccessRange<decltype((take.base_))>>{});
+        }
+        template<typename Take>
+        static auto begin_sized_(Take &take, std::false_type)
+        {
+            return make_counted_iterator(ranges::begin(take.base_), take.count_);
+        }
 
-        adaptor<simple_view<Rng>()> begin_adaptor()
+        template<typename Take>
+        static auto end_random_access_(Take &take, std::true_type)
         {
-            return {};
+            return ranges::begin(take.base_) + static_cast<range_difference_t<Rng>>(take.size());
         }
-        sentinel_adaptor<simple_view<Rng>()> end_adaptor()
+        static auto end_random_access_(detail::any, std::false_type)
         {
-            return {};
+            return default_sentinel;
         }
-        template<typename BaseRng = Rng>
-        auto begin_adaptor() const ->
-            CPP_ret(adaptor<true>)(
-                requires Range<BaseRng const>)
+        template<typename Take>
+        static auto end_sized_(Take &take, std::true_type, std::false_type) // sized
         {
-            return {};
+            return end_random_access_(
+                take,
+                meta::bool_<RandomAccessRange<decltype((take.base_))>>{});
         }
-        template<typename BaseRng = Rng>
-        auto end_adaptor() const ->
-            CPP_ret(sentinel_adaptor<true>)(
-                requires Range<BaseRng const>)
+        static auto end_sized_(detail::any, std::false_type, std::true_type) // infinite
         {
-            return {};
+            return default_sentinel;
         }
+        static auto end_sized_(take_view &take, std::false_type, std::false_type)
+        {
+            return sentinel<false>{ranges::end(take.base_)};
+        }
+        static auto end_sized_(take_view const &take, std::false_type, std::false_type)
+        {
+            return sentinel<true>{ranges::end(take.base_)};
+        }
+#endif
     public:
         take_view() = default;
-        take_view(Rng rng, range_difference_t<Rng> n)
-          : take_view::view_adaptor(std::move(rng)), n_{n}
+
+        constexpr take_view(Rng base, range_difference_t<Rng> count)
+          : base_(std::move(base))
+          , count_(count)
+        {}
+
+        constexpr Rng base() const
         {
-            RANGES_EXPECT(n >= 0);
+            return base_;
+        }
+
+        CPP_member
+        constexpr auto CPP_fun(begin)() (
+            requires (!simple_view<Rng>()))
+        {
+#if RANGES_CXX_IF_CONSTEXPR >= RANGES_CXX_IF_CONSTEXPR_17
+            if constexpr(SizedRange<Rng>)
+                if constexpr(RandomAccessRange<Rng>)
+                    return ranges::begin(base_);
+                else
+                {
+                    auto s = static_cast<range_difference_t<Rng>>(size());
+                    return make_counted_iterator(ranges::begin(base_), s);
+                }
+            else
+                return make_counted_iterator(ranges::begin(base_), count_);
+#else
+            return begin_sized_(*this, meta::bool_<SizedRange<Rng>>{});
+#endif
+        }
+
+        CPP_member
+        constexpr auto CPP_fun(begin)() (const
+            requires Range<Rng const>)
+        {
+#if RANGES_CXX_IF_CONSTEXPR >= RANGES_CXX_IF_CONSTEXPR_17
+            if constexpr(SizedRange<Rng const>)
+                if constexpr(RandomAccessRange<Rng const>)
+                    return ranges::begin(base_);
+                else
+                {
+                    auto s = static_cast<range_difference_t<Rng>>(size());
+                    return make_counted_iterator(ranges::begin(base_), s);
+                }
+            else
+                return make_counted_iterator(ranges::begin(base_), count_);
+#else
+            return begin_sized_(*this, meta::bool_<SizedRange<Rng const>>{});
+#endif
+        }
+
+        CPP_member
+        constexpr auto CPP_fun(end)() (
+            requires (!simple_view<Rng>()))
+        {
+#if RANGES_CXX_IF_CONSTEXPR >= RANGES_CXX_IF_CONSTEXPR_17
+            if constexpr(SizedRange<Rng>)
+                if constexpr(RandomAccessRange<Rng>)
+                    return ranges::begin(base_) + static_cast<range_difference_t<Rng>>(size());
+                else
+                    return default_sentinel;
+            // Not to spec: Infinite ranges:
+            else if constexpr(is_infinite<Rng>::value)
+                return default_sentinel;
+            else
+                return sentinel<false>{ranges::end(base_)};
+#else
+            return end_sized_(
+                *this,
+                meta::bool_<SizedRange<Rng>>{},
+                is_infinite<Rng>{});
+#endif
+        }
+
+        CPP_member
+        constexpr auto CPP_fun(end)() (const
+            requires Range<Rng const>)
+        {
+#if RANGES_CXX_IF_CONSTEXPR >= RANGES_CXX_IF_CONSTEXPR_17
+            if constexpr(SizedRange<Rng const>)
+                if constexpr(RandomAccessRange<Rng const>)
+                    return ranges::begin(base_) + static_cast<range_difference_t<Rng>>(size());
+                else
+                    return default_sentinel;
+            // Not to spec: Infinite ranges:
+            else if constexpr(is_infinite<Rng const>::value)
+                return default_sentinel;
+            else
+                return sentinel<true>{ranges::end(base_)};
+#else
+            return end_sized_(
+                *this,
+                meta::bool_<SizedRange<Rng const>>{},
+                is_infinite<Rng const>{});
+#endif
+        }
+
+        CPP_member
+        constexpr auto CPP_fun(size)() (requires SizedRange<Rng>)
+        {
+            auto n = ranges::size(base_);
+            return ranges::min(n, static_cast<decltype(n)>(count_));
+        }
+        CPP_member
+        constexpr auto CPP_fun(size)() (const requires SizedRange<Rng const>)
+        {
+            auto n = ranges::size(base_);
+            return ranges::min(n, static_cast<decltype(n)>(count_));
         }
     };
+
+#if RANGES_CXX_DEDUCTION_GUIDES >= RANGES_CXX_DEDUCTION_GUIDES_17
+    CPP_template(typename Rng)(
+        requires ViewableRange<Rng>)
+    take_view(Rng &&, range_difference_t<Rng>) ->
+        take_view<view::all_t<Rng>>;
+#endif
 
     namespace view
     {
@@ -108,23 +255,6 @@ namespace ranges
         {
         private:
             friend view_access;
-
-            template<typename Rng>
-            static auto impl_(Rng &&rng, range_difference_t<Rng> n) ->
-                CPP_ret(take_view<all_t<Rng>>)(
-                    requires not SizedRange<Rng> && !is_infinite<Rng>::value)
-            {
-                return {all(static_cast<Rng &&>(rng)), n};
-            }
-
-            template<typename Rng>
-            static auto CPP_fun(impl_)(Rng &&rng, range_difference_t<Rng> n)(
-                requires SizedRange<Rng> || is_infinite<Rng>::value)
-            {
-                return take_exactly(
-                    static_cast<Rng &&>(rng),
-                    is_infinite<Rng>::value ? n : ranges::min(n, distance(rng)));
-            }
 
             template<typename Int>
             static auto CPP_fun(bind)(take_fn take, Int n)(
@@ -135,10 +265,11 @@ namespace ranges
 
         public:
             template<typename Rng>
-            auto CPP_fun(operator())(Rng &&rng, range_difference_t<Rng> n) (const
-                requires ViewableRange<Rng> && InputRange<Rng>)
+            auto operator()(Rng &&rng, range_difference_t<Rng> n) const ->
+                CPP_ret(take_view<all_t<Rng>>)(
+                    requires ViewableRange<Rng>)
             {
-                return take_fn::impl_(static_cast<Rng &&>(rng), n);
+                return {all(static_cast<Rng &&>(rng)), n};
             }
         };
 
