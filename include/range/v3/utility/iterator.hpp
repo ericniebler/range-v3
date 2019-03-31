e339cdcc98ab8f29ea13f0383373eaa873bc3590
tower120 <tower120@gmail.com>
2019-03-31 18:57:26 +0300

    Update iterator.hpp

--- include/range/v3/utility/iterator.hpp
+++ include/range/v3/utility/iterator.hpp
@@ -215,7 +215,7 @@ namespace ranges
                 RANGES_EXPECT((Same<I, S>() || 0 <= n));
                 if (n == 0) return 0;
                 D d = bound - it;
-                RANGES_EXPECT(n == 0 || 0 < n ? 0 <= d : 0 >= d);
+                RANGES_EXPECT(0 <= n ? 0 <= d : 0 >= d);
                 if(0 <= n ? n >= d : n <= d)
                 {
                     ranges::advance(it, std::move(bound));
