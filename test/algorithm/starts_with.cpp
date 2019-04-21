ff4d80019c5374a5b1b4dd38c30a46b8f4fef150
Christopher Di Bella <cjdb.ns@gmail.com>
2019-04-21 20:28:56 +0100

    adds `starts_with` algorithm
    Closes #1150
    
    C++20 will see `basic_string::starts_with`. This commit adds a more
    general `starts_with` that is appropriate for any two input ranges.

---
+++ test/algorithm/starts_with.cpp
@@ -0,0 +1,119 @@
+// Range v3 library
+//
+//  Copyright 2019 Christopher Di Bella
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+#include <range/v3/algorithm/starts_with.hpp>
+
+#include "../simple_test.hpp"
+#include "../test_iterators.hpp"
+#include <forward_list>
+#include <range/v3/range/conversion.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/slice.hpp>
+#include <range/v3/view/take_exactly.hpp>
+#include <range/v3/view/istream.hpp>
+#include <sstream>
+#include <vector>
+
+void test_defaults()
+{
+   using namespace ranges;
+   { // checks starts_with works for input ranges
+      constexpr auto full_latin_alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+      auto const partial_latin_alphabet = "a b c d";
+
+      {
+         auto long_stream = std::istringstream{full_latin_alphabet};
+         auto short_stream = std::istringstream{partial_latin_alphabet};
+
+         auto r1 = istream<char>(long_stream);
+         auto r2 = istream<char>(short_stream);
+         CHECK(starts_with(begin(r1), end(r1), begin(r2), end(r2)));
+      }
+      {
+         auto long_stream = std::istringstream{full_latin_alphabet};
+         auto short_stream = std::istringstream{partial_latin_alphabet};
+
+         auto r1 = istream<char>(long_stream);
+         auto r2 = istream<char>(short_stream);
+         CHECK(not starts_with(begin(r2), end(r2), begin(r1), end(r1)));
+      }
+      {
+         auto long_stream = std::istringstream{full_latin_alphabet};
+         auto short_stream = std::istringstream{partial_latin_alphabet};
+         CHECK(starts_with(istream<char>(long_stream), istream<char>(short_stream)));
+      }
+      {
+         auto long_stream = std::istringstream{full_latin_alphabet};
+         auto short_stream = std::istringstream{partial_latin_alphabet};
+         CHECK(not starts_with(istream<char>(short_stream), istream<char>(long_stream)));
+      }
+   }
+   { // checks starts_with works for random-access ranges
+      auto const long_range = view::iota(0, 100) | to<std::vector>;
+      auto const short_range = view::iota(0, 10) | to<std::vector>;
+
+      CHECK(starts_with(begin(long_range), end(long_range), begin(short_range), end(short_range)));
+      CHECK(starts_with(long_range, short_range));
+
+      CHECK(not starts_with(begin(short_range), end(short_range), begin(long_range), end(long_range)));
+      CHECK(not starts_with(short_range, long_range));
+   }
+   { // checks starts_with works for random-access ranges with arbitrary sentinels
+      auto const long_range = view::iota(0);
+      auto const short_range = view::iota(0) | view::take_exactly(100);
+
+      CHECK(starts_with(begin(long_range), end(long_range), begin(short_range), end(short_range)));
+      CHECK(starts_with(long_range, short_range));
+
+      CHECK(not starts_with(begin(short_range), end(short_range), begin(long_range), end(long_range)));
+      CHECK(not starts_with(short_range, long_range));
+   }
+   { // checks starts_with identifies a subrange
+      auto const range = view::iota(0) | view::slice(50, 100);
+      auto const offset = view::iota(50, 100);
+
+      CHECK(starts_with(begin(range), end(range), begin(offset), end(offset)));
+      CHECK(starts_with(range, offset));
+
+      CHECK(starts_with(begin(offset), end(offset), begin(range), end(range)));
+      CHECK(starts_with(offset, range));
+   }
+   { // checks starts_with identifies when two ranges don't have the same start sequence
+      auto const first = view::iota(0, 1'000);
+      auto const second = view::iota(10, 1'000);
+
+      CHECK(not starts_with(begin(first), end(first), begin(second), end(second)));
+      CHECK(not starts_with(first, second));
+
+      CHECK(not starts_with(begin(second), end(second), begin(first), end(first)));
+      CHECK(not starts_with(second, first));
+   }
+}
+
+void test_comparison()
+{
+   using namespace ranges;
+   auto const long_range = view::iota(0, 100);
+   auto const short_range = view::iota(1, 51);
+   CHECK(starts_with(begin(long_range), end(long_range), begin(short_range), end(short_range),
+      less{}));
+   CHECK(starts_with(long_range, short_range, less{}));
+
+   CHECK(not starts_with(begin(long_range), end(long_range), begin(short_range), end(short_range),
+      greater{}));
+   CHECK(not starts_with(long_range, short_range, greater{}));
+}
+
+int main()
+{
+   ::test_defaults();
+   ::test_comparison();
+   return ::test_result();
+}
