49ac73372ab437e32000907259bfa8df53fb4a3e
tower120 <tower120@gmail.com>
2019-04-23 22:07:22 +0300

    diagnostic hack

--- test/view/compose.cpp
+++ test/view/compose.cpp
@@ -26,8 +26,11 @@
 
 using namespace ranges;
 
+RANGES_DIAGNOSTIC_PUSH
+RANGES_DIAGNOSTIC_IGNORE_GLOBAL_CONSTRUCTORS
 using List = std::vector<std::unique_ptr<int>>;
 List list;
+RANGES_DIAGNOSTIC_POP
 
 struct nullary_test
 {
