795c198d9de9c937f86bceb157e52bbc8184886e
Robert Andrzejuk <robert-andrzejuk@users.noreply.github.com>
2019-05-12 16:16:30 +0200

    Fix to_container.hpp: MSVC 2019 declaration of 'size' hides global declaration
    MSVC 2019:
    1>\range\v3\to_container.hpp(76,1): warning C4459:  declaration of 'size' hides global declaration
    1>\range-v3\include\range\v3\to_container.hpp(76,1): warning C4459:                     auto const size = ranges::size(rng);
    1>\range\v3\size.hpp(112,13): message :  see declaration of 'ranges::v3::CPOs::size'
    1>\range\v3\size.hpp(112,13): message :             RANGES_INLINE_VARIABLE(_size_::fn, size)

--- include/range/v3/to_container.hpp
+++ include/range/v3/to_container.hpp
@@ -73,13 +73,13 @@ namespace ranges
                 Cont impl(Rng &&rng, std::true_type) const
                 {
                     Cont c;
-                    auto const size = ranges::size(rng);
+                    auto const rng_size = ranges::size(rng);
                     using size_type = decltype(c.max_size());
                     using C = common_type_t<
-                        meta::_t<std::make_unsigned<decltype(size)>>,
+                        meta::_t<std::make_unsigned<decltype(rng_size)>>,
                         meta::_t<std::make_unsigned<size_type>>>;
-                    RANGES_EXPECT(static_cast<C>(size) <= static_cast<C>(c.max_size()));
-                    c.reserve(static_cast<size_type>(size));
+                    RANGES_EXPECT(static_cast<C>(rng_size) <= static_cast<C>(c.max_size()));
+                    c.reserve(static_cast<size_type>(rng_size));
                     using I = range_common_iterator_t<Rng>;
                     c.assign(I{ranges::begin(rng)}, I{ranges::end(rng)});
                     return c;
