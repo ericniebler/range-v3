1d67213a694b433e1103ebb4927720b8fc71ec16
Barry Revzin <barry.revzin@gmail.com>
2019-06-18 20:58:40 -0500

    Adding take_last.

---
+++ include/range/v3/view/take_last.hpp
@@ -0,0 +1,51 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Eric Niebler 2013-present
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+
+#ifndef RANGES_V3_VIEW_TAKE_LAST_HPP
+#define RANGES_V3_VIEW_TAKE_LAST_HPP
+
+#include <range/v3/view/drop_exactly.hpp>
+
+namespace ranges
+{
+    namespace view
+    {
+        struct take_last_fn
+        {
+        private:
+            friend view_access;
+            template<typename Int>
+            static auto CPP_fun(bind)(take_last_fn take_last, Int n)( //
+                requires Integral<int>)
+            {
+                return make_pipeable(std::bind(take_last, std::placeholders::_1, n));
+            }
+
+        public:
+            template<typename Rng>
+            auto CPP_fun(operator())(Rng && rng, range_difference_t<Rng> n)(
+                const requires ViewableRange<Rng> && InputRange<Rng> && SizedRange<Rng>)
+            {
+                range_difference_t<Rng> sz = ranges::size(rng);
+                return drop_exactly(static_cast<Rng &&>(rng), sz > n ? sz - n : 0);
+            }
+        };
+
+        /// \relates take_last_fn
+        /// \ingroup group-views
+        RANGES_INLINE_VARIABLE(view<take_last_fn>, take_last)
+    } // namespace view
+    /// @}
+} // namespace ranges
+
+#endif
---
+++ test/view/take_last.cpp
@@ -0,0 +1,34 @@
+// Range v3 library
+//
+//  Copyright Eric Niebler 2014-present
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+
+#include <range/v3/utility/copy.hpp>
+#include <range/v3/view/take_last.hpp>
+
+#include "../simple_test.hpp"
+#include "../test_utils.hpp"
+
+int main()
+{
+    using namespace ranges;
+
+    int rgi[] = {0, 1, 2, 3, 4, 5};
+
+    auto rng0 = rgi | view::take_last(3);
+    has_type<int &>(*begin(rng0));
+    models<BoundedViewConcept>(aux::copy(rng0));
+    models<SizedViewConcept>(aux::copy(rng0));
+    models<RandomAccessIteratorConcept>(begin(rng0));
+    ::check_equal(rng0, {3, 4, 5});
+    CHECK(size(rng0) == 3u);
+
+    auto rng1 = rgi | view::take_last(7);
+    ::check_equal(rng1, {0, 1, 2, 3, 4, 5});
+}
