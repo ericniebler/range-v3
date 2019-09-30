f4249d39c8923fd5d65246978842cc4c5bf50519
Casey Carter <Casey@Carter.net>
2019-09-30 08:50:47 -0700

    Format regression test in range-v3 style

--- test/bug1335.cpp
+++ test/bug1335.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 #include <range/v3/action/sort.hpp>
 
-template <class A, class B>
-constexpr auto operator-(A a, B) {
-  return a;
+template<typename A, typename B>
+constexpr auto operator-(A a, B)
+{
+    return a;
 }
 
-int main() {
-  std::vector<int> data;
-  data |= ranges::actions::sort;
+int main()
+{
+    std::vector<int> data;
+    data |= ranges::actions::sort;
 }
