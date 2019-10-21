f801257bac628900b2d1237d06a9a94dc7108805
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2019-10-20 22:35:41 -0400

    Fix license version

--- test/algorithm/ends_with.cpp
+++ test/algorithm/ends_with.cpp
@@ -3,9 +3,9 @@
 //  Copyright Johel Guerrero 2019
 //
 //  Use, modification and distribution is subject to the
-//  Boost Software License, Version 0.0. (See accompanying
-//  file LICENSE_0_0.txt or copy at
-//  http://www.boost.org/LICENSE_0_0.txt)
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 //
 // Project home: https://github.com/ericniebler/range-v3
 //
