a7d8535c8b4513e7ea3559e9389328de0785ce64
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-05-22 20:47:56 +0200

    assignable_from: use the definition from https://eel.is/c++draft/concept.assignable

--- include/concepts/concepts.hpp
+++ include/concepts/concepts.hpp
@@ -1013,6 +1013,7 @@ namespace concepts
         template<typename T, typename U>
         CPP_concept_bool assignable_from =
             std::is_lvalue_reference<T>::value &&
+            common_reference_with<std::remove_reference_t<T> const &, std::remove_reference_t<U> const &> &&
             CPP_fragment(defs::assignable_from_, T, U);
 
         template<typename T>
