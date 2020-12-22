ca552fe9d1b76c35fc2e3915ba54856688d82f6d
Semir Vrana <semir.vrana@gmx.de>
2020-12-22 21:47:11 +0100

    Rename (disable|enable)_warnings to their new names

--- include/range/v3/view/successor.hpp
+++ include/range/v3/view/successor.hpp
@@ -31,7 +31,7 @@
 #include <range/v3/utility/static_const.hpp>
 #include <range/v3/view/facade.hpp>
 
-#include <range/v3/detail/disable_warnings.hpp>
+#include <range/v3/detail/prologue.hpp>
 
 namespace ranges
 {
@@ -109,7 +109,7 @@ namespace ranges
       /// \@}
 } // namespace ranges
 
-#include <range/v3/detail/reenable_warnings.hpp>
+#include <range/v3/detail/epilogue.hpp>
 #include <range/v3/detail/satisfy_boost_range.hpp>
 RANGES_SATISFY_BOOST_RANGE(::ranges::successor_view)
 
