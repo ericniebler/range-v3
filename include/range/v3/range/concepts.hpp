40bbf568c9cb8e274ab9c054af67fcf60a854245
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-10-06 16:52:42 +0200

    relax ranges::view_ concept

--- include/range/v3/range/concepts.hpp
+++ include/range/v3/range/concepts.hpp
@@ -207,7 +207,7 @@ namespace ranges
     template<typename T>
     CPP_concept view_ =
         range<T> &&
-        semiregular<T> &&
+        copyable<T> &&
         enable_view<T>;
 
     template<typename T>
