fd99d18aaf25a4684d0d67cb2161d9349d65f590
refvalue <metabeyond@outlook.com>
2023-03-14 10:43:11 +0800

    fixed the formatting.

--- include/range/v3/iterator/concepts.hpp
+++ include/range/v3/iterator/concepts.hpp
@@ -95,7 +95,7 @@ namespace ranges
             requires same_as<I, class I::_String_view_iterator>)
         auto iter_concept_(I, priority_tag<3>)
             -> ranges::contiguous_iterator_tag;
-		template(typename I)(
+        template(typename I)(
             requires same_as<I, class I::_Span_iterator>)
         auto iter_concept_(I, priority_tag<3>)
             -> ranges::contiguous_iterator_tag;
