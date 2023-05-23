9aa41d6b8ded2cf5e8007e66a0efd1ab33dbf9a5
Alexander <Smertig@users.noreply.github.com>
2023-05-23 16:10:23 +0300

    Fix include guard name (#1777)

---
+++ test/bug-guard.cpp
@@ -0,0 +1,19 @@
+// Range v3 library
+//
+//  Copyright Eric Niebler 2014-present
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+
+#include <range/v3/iterator.hpp>
+#include <range/v3/utility.hpp>
+
+int main()
+{
+    // make sure, that `utility.hpp` is included correctly
+    ranges::optional<int> a;
+}
